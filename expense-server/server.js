f4b4c8196af7c551c6531d719b1e8ae7de4c1410
Marouf Mushtaq <maroufwani23@gmail.com>
2026-01-24 12:17:33 +0530

    add files

--- expense-server/server.js
+++ expense-server/server.js
@@ -1,61 +1,11 @@
 const express = require('express');
+const authRoutes = require('./src/routes/authRoutes');
+
 const app = express();
 
 app.use(express.json());
 
-const users = [];
-
-app.post('/register', (req, res) => {
-    const { username, email, password } = req.body;
-
-    if (!username || !email || !password) {
-        return res.status(400).json({ 
-            message: 'Please provide username, email, and password' 
-        });
-    }
-
-    const existingUser = users.find(u => u.email === email);
-    if (existingUser) {
-        return res.status(400).json({ 
-            message: `user already exists with email: ${email}` 
-        });
-    }
-    const newUser = {
-        id: users.length+1,
-        username: username,
-        email: email,
-        password: password
-    }
-    users.push(newUser);
-
-    console.log('New user registration:', { username, email });
-    console.log('All users:', users);
-
-    res.status(201).json({ 
-        message: 'User registered successfully',
-        user: { id: newUser.id, username, email }
-    });
-});
-
-app.post('/login', (req,res)=>{
-    const {email,password} = req.body;
-    if(!email || !password){
-        return res.status(400).json({
-            message: "Please enter email and password"
-        })
-    }
-    const user = users.find(u=>u.email==email&&u.password==password)
-    if (!user){
-        return res.status(400).json({
-            message: "Invalid email or password"
-        })
-    }
-    else{
-        res.status(200).json({
-            message: `Welcome back, ${user.username}`
-        })
-    }
-})
+app.use('/auth', authRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
---
+++ expense-server/src/dao/userDB.js
@@ -0,0 +1,7 @@
+/**
+ * To be deleted when we integrate with MongoDB
+ */
+
+const users = [];
+
+module.exports = users;
